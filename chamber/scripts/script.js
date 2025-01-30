0536403efa5b30eac8a832b38be47b18928169a2
Greice Moreira <greice.f.d.a@hotmail.com>
2025-01-30 10:03:01 -0500

    json

---
+++ chamber/scripts/script.js
@@ -0,0 +1,67 @@
+const lastModification = document.getElementById("lastModification");
+const d = new Date();
+
+function thisYear() {
+    document.getElementById("currentyear").innerHTML = "&copy;" + d.getFullYear() + " | 👩🏼‍💻 Greice Moreira | Rio Grande do Sul, Brazil"
+}
+
+function myFunction() {
+    lastModification.innerHTML = "Last modification: " + document.lastModified
+}
+
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('show');
+}
+
+function onLoad() {
+    thisYear()
+    myFunction()
+}
+
+async function getData(){
+    const company = await fetch()
+
+}
+
+const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
+const cards = document.querySelector('#cards');
+
+async function getProphetData(){
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+
+const displayProphets = (prophets) => {
+    prophets.forEach(prophet => {
+        let card = document.createElement('section');
+        card.classList.toggle('own');
+        let fullName = document.createElement('h2');
+        let birthDate = document.createElement('h3');
+        let birthPlace = document.createElement('h4');
+        let portrait = document.createElement('img');
+
+        fullName.textContent = `${prophet.name} prophet`;
+
+        birthDate.textContent = `${prophet.birthdate}`;
+        birthPlace.textContent = `${prophet.birthplace}`;
+
+
+        portrait.setAttribute('src' , prophet.imageurl);
+        portrait.setAttribute('alt' , 'Portrait of ${prophet.name} prophet');
+        portrait.setAttribute('loading' , 'lazy');
+        portrait.setAttribute('width' , '340');
+        portrait.setAttribute( 'heigth' , '440');
+
+        card.appendChild(fullName)
+        card.appendChild(birthDate)
+        card.appendChild(birthPlace)
+        card.appendChild(portrait)
+
+        cards.appendChild(card)
+    });
+}
+
+getProphetData();
