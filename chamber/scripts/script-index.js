65099b457fe31e4b0ac191d6e65abc1516bf9b6c
Greice Moreira <greice.f.d.a@hotmail.com>
2025-02-10 10:48:11 -0500

    aa

--- chamber/scripts/script-index.js
+++ chamber/scripts/script-index.js
@@ -256,3 +256,18 @@ const displayWeatherForecast = (result) => {
 
     forecastW.appendChild(card)
 }
+
+// Function to get URL parameters
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param) || "Not provided";
+}
+
+// Populate fields with form data
+document.getElementById("firstName").textContent = getQueryParam("first-name");
+document.getElementById("lastName").textContent = getQueryParam("last-name");
+document.getElementById("email").textContent = getQueryParam("email");
+document.getElementById("mobile").textContent = getQueryParam("phone");
+document.getElementById("business").textContent = getQueryParam("org-name");
+document.getElementById("timestamp").textContent = getQueryParam("timestamp");
+
